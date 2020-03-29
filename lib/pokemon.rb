4da3e2594602d658861daf38323247ebb6cbd366
Xiaoyi Liu <lxy878@gmail.com>
2020-03-29 11:36:50 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,7 @@
 class Pokemon
   attr_accessor :name, :type
   attr_reader :id, :db
-  def initialize(id=nil, name, type, db)
+  def initialize(id: nil, name:, type:, db:)
     @id = id
     @name = name
     @type = type
