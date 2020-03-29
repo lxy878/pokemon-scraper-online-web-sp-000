793777a591dcc67bfbcaec0fc93a667acacf0f58
Xiaoyi Liu <lxy878@gmail.com>
2020-03-29 11:49:27 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -22,7 +22,7 @@ class Pokemon
       SELECT * FROM pokemon WHERE id = ?
     SQL
     pokemon_array = db.execute(sql, id)
-    pokemon_array.collect {|data| Pokemon.new(data[0], data[1], data[2], db)}.first
+    pokemon_array.collect {|data| Pokemon.new(id: data[0], name: data[1], type: data[2], db:db)}.first
   end
 
 end
